bbf9b84a849186eaf2416edf3df3fa074b00ef17
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2023-12-28 20:37:54 +0200

    new: functions for students

---
+++ main.py
@@ -0,0 +1,82 @@
+import sqlite3
+
+conn = sqlite3.connect("university.db")
+
+cursor = conn.cursor()
+
+
+while True:
+    print("\n1. Додати нового студента")
+    print("2. Додати новий курс")
+    print("3. Показати список студентів")
+    print("4. Показати список курсів")
+    print("5. Зареєструвати студента на курс")
+    print("6. Показати студентів на конкретному курсі")
+    print("7. Вийти")
+
+    choice = input("Оберіть опцію (1-7): ")
+
+    if choice == "1":
+        # Додавання нового студента
+        name = input("Ім'я")
+        age = input("Вік")
+        major = input("Спеціальність")
+
+        cursor.execute('''
+        INSERT INTO students (name, age, major) VALUES(?, ?, ?)
+        ''', (name, age, major))
+        conn.commit()
+
+    elif choice == "2":
+        # Додавання нового курсу
+        course_name = input("Назва курсу")
+        instructor = input("Викладач")
+
+        cursor.execute('''
+        INSERT INTO courses (course_name, instructor) VALUES(?, ?, ?)
+        ''')
+        conn.commit()
+    elif choice == "3":
+        # Показати список студентів
+        cursor.execute('''
+        SELECT name FROM students
+        ''')
+        students = cursor.fetchall()
+        if students:
+            print("Список студентів:")
+            for student in students:
+                print(student[0])
+    elif choice == "4":
+        # Показати список курсів
+        cursor.execute('''
+        SELECT course_name FROM courses
+        ''')
+        courses = cursor.fetchall()
+        if courses:
+            print("Список студентів:")
+            for course in courses:
+                print(course[0])
+
+    elif choice == "5":
+        # Зареєструвати студента на курс
+        student_id = input("Введіть ID студента:")
+        course_id = input("Введіть ID курсу:")
+
+        cursor.execute('''
+        INSERT INTO student_courses (student_id, course_id) VALUES(?, ?)
+        ''', (student_id, course_id))
+
+        conn.commit()
+    elif choice == "6":
+        # Показати студентів на конкретному курсі
+        course_id = input("Введіть ID курсу:")
+        cursor.execute('''
+        SELECT students.id, students.name FROM students, student_courses WHERE students.id = student_courses.student_id AND student_courses.course_id = ?
+        ''', (course_id))
+        students_on_course = cursor.fetchall()
+        print(students_on_course)
+    elif choice == "7":
+        break
+
+    else:
+        print("Некоректний вибір. Будь ласка, введіть число від 1 до 7.")
